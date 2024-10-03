0e9e2960c725b7a95f41374a649ea2febe1e99dc
Kage <dumitruwebdev@gmail.com>
2024-10-03 16:49:41 +0300

    update

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const PORT = 1000 || process.env.PORT;
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173 https://social-media-mern-stack-client.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:5173', 'https://social-media-mern-stack-client.vercel.app']);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
