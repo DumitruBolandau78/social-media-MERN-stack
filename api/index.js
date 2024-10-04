e795cda171ad72263fe6f6243ef0a1db229cd813
Kage <dumitruwebdev@gmail.com>
2024-10-04 21:48:41 +0300

    frc

--- api/index.js
+++ api/index.js
@@ -59,7 +59,7 @@ app.use(
     session({
         secret: 'your-secret-key',
         resave: false,
-        saveUninitialized: false,
+        saveUninitialized: true,
         store,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 30, // Session expiry time (optional)
