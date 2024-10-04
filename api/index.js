d2835640efe9cf02d63ae5a38f77db46e19b0ad4
Kage <dumitruwebdev@gmail.com>
2024-10-05 02:27:34 +0300

    tret

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(cors({
     origin: ['https://social-media-mern-stack-client.vercel.app', 'http://localhost:5173'],
     methods: ['get', 'post', 'put', 'delete'],
